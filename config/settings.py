55468de3a7962bf8d2163d90e53204570b453a27
Aminjon <aminrustamov0424@gmail.com>
2026-05-21 22:12:50 +0500

    fix media file upload issue

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'media')
